9910c226442ec5901b8c1c6dcefcd83ce8e02001
黄凌凌 <594675849@qq.com>
2019-04-10 16:19:27 +0800

    信息安全学实验2

--- 信息安全学实验2/信息安全学实验2.cpp
+++ 信息安全学实验2/信息安全学实验2.cpp
@@ -1,20 +1,66 @@
-﻿// 信息安全学实验2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include<iostream>
+#include<string>
+#include<fstream>
+#include <sstream>
+using namespace std;
 
-#include <iostream>
+string readFileIntoString(char* filename)
+{
+	ifstream ifile(filename);
+	//将文件读入到ostringstream对象buf中
+	ostringstream buf;
+	char ch;
+	while (buf && ifile.get(ch))
+		buf.put(ch);
+	//返回与流对象buf关联的字符串
+	return buf.str();
+}
 
 int main()
 {
-    std::cout << "Hello World!实验2000\n"; 
+	cout << "------位移密码解密-------" << endl;
+	//文件名
+	char* fn = "a.txt";
+	string str,strs;
+	str = readFileIntoString(fn);
+	strs = str;
+	cout << "要解密的文件数据为：";
+	cout << str << endl;
+	int flag[256];
+	int kx=0;//密钥
+	int max = 0;
+	for (int i = 0; i < (int)str.length(); i++)
+	{
+		flag[i] = 0;//每一个字符有一个标志位，为它出现的次数。
+		for (int j = 0; j <(int)str.length(); j++)
+		{
+			if(str[i]=str[j])
+			{
+				flag[i] = flag[i] + 1;
+			}
+		}
+
+	}
+	for (int i = 1; i <(int)str.length(); i++)
+	{
+		if (flag[i] > flag[max])
+			max = i;
+	}
+
+	kx = str[max]-'E';
+
+	cout <<"频度分析生成是密钥："<< kx << endl;		
+	for (int i = 0; i <(int) strs.length();i++)//解密过程
+		{
+			if (strs[i] >= 'A' && strs[i] <= 'Z')
+			{
+				strs[i] = (strs[i] - 'A' - kx) % 26 + 'A'+32;
+				if (strs[i] < 97)
+					strs[i] = strs[i] + 26;//防止出现其他特殊字符
+			}
+		}
+		cout << "解密结果：";
+		cout << strs << endl;
+	system("pause");
+
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
